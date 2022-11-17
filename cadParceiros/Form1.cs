b3b87b59180adf69ccc1fbc94f6091850be5ffbd
DenerBarros <denerbarros.eng@gmail.com>
2022-11-17 19:28:51 -0300

    Criado Parte do Formulário

---
+++ cadParceiros/Form1.cs
@@ -0,0 +1,40 @@
+namespace cadParceiros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
