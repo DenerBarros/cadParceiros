172ab701a4a2115371ab4a1cd8e9079ee245d16b
DenerBarros <denerbarros.eng@gmail.com>
2022-11-18 10:16:12 -0300

    Criado as classes de Parceiro e Endereço

---
+++ cadParceiros/Endereco.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadParceiros
+{
+    internal class Endereco
+    {      
+
+        public string CepC { get; set; }
+        public string LogradouroC { get; set; }
+        public int NumeroC { get; set; }
+        public int BairroC { get; set; }
+        public string CidadeC { get; set; }
+        public string EstadoC { get; set; }
+        public string CepE { get; set; }
+        public string LogradouroE { get; set; }
+        public int NumeroE { get; set; }
+        public int BairroE { get; set; }
+        public string CidadeE { get; set; }
+        public string EstadoE { get; set; }
+
+        public Endereco(string cepC, string logradouroC, int numeroC, int bairroC, string cidadeC, string estadoC, string cepE, string logradouroE, int numeroE, int bairroE, string cidadeE, string estadoE)
+        {
+            CepC = cepC;
+            LogradouroC = logradouroC;
+            NumeroC = numeroC;
+            BairroC = bairroC;
+            CidadeC = cidadeC;
+            EstadoC = estadoC;
+            CepE = cepE;
+            LogradouroE = logradouroE;
+            NumeroE = numeroE;
+            BairroE = bairroE;
+            CidadeE = cidadeE;
+            EstadoE = estadoE;
+        }
+
+    }
+}
---
+++ cadParceiros/Parceiro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadParceiros
+{
+    public class Parceiro
+    {      
+
+        public string Nome { get; set; }
+        public string EmailSolicitacao { get; set; }
+        public long Cpfcnpj { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+
+        public Parceiro(string nome, string emailSolicitacao, long cpfcnpj, string email, string telefone)
+        {
+            this.Nome = nome;
+            this.EmailSolicitacao = emailSolicitacao;
+            this.Cpfcnpj = cpfcnpj;
+            this.Email = email;
+            this.Telefone = telefone;
+        }
+
+
+    }
+}
